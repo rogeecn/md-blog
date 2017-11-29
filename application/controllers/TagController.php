6877d0a60773de893bd6f393bb4bd896ba045b38
yanghao <yanghao@b.360.cn>
2017-11-29 16:38:26 +0800

    -modify prod configurations

--- application/controllers/TagController.php
+++ application/controllers/TagController.php
@@ -3,22 +3,20 @@ namespace application\controllers;
 
 use application\base\BaseController;
 use common\extend\Pagination;
-use common\models\Post;
 use common\models\Tag;
-use common\utils\UserSession;
 use yii\data\ActiveDataProvider;
 
 class TagController extends BaseController
 {
     public function actionIndex()
     {
-        $query = Tag::find();
+        $query = Tag::find()->orderBy(['ref_count' => SORT_DESC]);
 
         $dataProvider = new ActiveDataProvider([
             'query'      => $query,
             'pagination' => [
                 'class'           => Pagination::className(),
-                'defaultPageSize' => $this->pageItemCount * 10,
+                'defaultPageSize' => $this->pageItemCount * 100,
             ],
         ]);
 
