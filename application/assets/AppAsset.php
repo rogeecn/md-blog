10652d7f931717bb27f2f606f4f5d06e5391f99c
rogeecn <rogeecn@gmail.com>
2017-12-01 09:58:35 +0800

    -remove code highlight

--- application/assets/AppAsset.php
+++ application/assets/AppAsset.php
@@ -13,10 +13,8 @@ class AppAsset extends AssetBundle
     public $baseUrl  = '@web';
     public $css      = [
         'css/site.css',
-        '//cdn.staticfile.org/highlight.js/9.12.0/styles/github.min.css',
     ];
     public $js       = [
-        "//cdn.staticfile.org/highlight.js/9.12.0/highlight.min.js",
     ];
     public $depends  = [
         'yii\bootstrap\BootstrapAsset',
